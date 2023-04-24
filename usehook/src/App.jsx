aae38bac7206a2a7f4b31c1134382728893bad44
Hoang Hong <honghoang@bincg.com>
2023-04-24 15:30:17 +0700

    continue

--- usehook/src/App.jsx
+++ usehook/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
+import ContextAPI from "./components/contextapi/ContextAPI";
 import Effect from "./components/effect/Effect";
 import Memo from "./components/memo/Memo";
 import State from "./components/state/State";
+import UseId from "./components/useid/UseId";
+import './App.css';
 function App() {
   return (
-    <div className="App">
+    <div>
       {/* <State /> */}
       {/* <Effect /> */}
-      <Memo />
+      {/* <Memo /> */}
+      {/* <UseId /> */}
+      <ContextAPI />
     </div>
   );
 }
---
+++ usehook/src/components/useid/UseId.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './Form';
+
+const UseId = () => {
+  return (
+    <div>
+      <Form />
+      {/* <Form />  */}
+    </div>
+  );
+};
+
+export default UseId;
